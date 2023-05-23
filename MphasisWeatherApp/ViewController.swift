37afe0e6f7d716c0c223e9665b3ab3aeed59c3f4
Preeti Priyam <ppriyam1@binghamton.edu>
2023-05-22 19:54:55 -0700

    initial project setup
    * set WeatherBaseController class as the entry point of the project

---
+++ MphasisWeatherApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MphasisWeatherApp
+//
+//  Created by Preeti Priyam on 5/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+
+}
+
---
+++ MphasisWeatherApp/WeatherBaseController.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherBaseController.swift
+//  MphasisWeatherApp
+//
+//  Created by Preeti Priyam on 5/22/23.
+//
+
+import Foundation
+import UIKit
+
+class WeatherBaseController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
